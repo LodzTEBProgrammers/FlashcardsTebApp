10071a6c43c07ef2e4f166a862b9071e61fd8821
nic00la <nic00la.kaleta@gmail.com>
2024-11-17 15:44:06 +0100

    adding and displaying flashcards (beta)

--- FlashcardsClient/src/app/app.routes.ts
+++ FlashcardsClient/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FlashcardListComponent } from './components/flashcard-list/flashcard-list.component';
+import { FlashcardCreateComponent } from './components/flashcard-create/flashcard-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FlashcardListComponent },
+  { path: 'create', component: FlashcardCreateComponent }
+];
---
+++ FlashcardsClient/src/app/models/flashcard.model.ts
@@ -0,0 +1,6 @@
+export interface Flashcard {
+  id: string;
+  term: string;
+  definition: string;
+  remembered: boolean;
+}
