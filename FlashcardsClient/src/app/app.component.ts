defdfcc566011ad5a35b244afba2d3f19c98afe8
nic00la1 <nic00la.kaleta@gmail.com>
2024-12-01 02:33:11 +0100

    navbar wyglad

--- FlashcardsClient/src/app/app.component.ts
+++ FlashcardsClient/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationComponent} from "./components/partials/navigation/navigation.component";
+import {NavbarComponent} from "./components/partials/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- FlashcardsClient/src/app/app.routes.ts
+++ FlashcardsClient/src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { FlashcardCreateComponent } from './components/pages/flashcard-create/fl
 import {FolderComponent} from "./components/pages/folder/folder.component";
 import {FlashcardSetComponent} from "./components/pages/flashcard-set/flashcard-set.component";
 import {UserResourcesComponent} from "./components/pages/user-resources/user-resources.component";
-import {HomePageComponent} from "./components/pages/home-page/home-page.component";
+import {HomeComponent} from "./components/pages/home/home.component";
 import {RegisterComponent} from "./components/pages/register/register.component";
 import {LoginComponent} from "./components/pages/login/login.component";
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'create', component: FlashcardCreateComponent },
